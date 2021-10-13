4ab42de2722577c503eafdc29e844b54813e8c19
Alexandre Pereira <lexandre32@gmail.com>
2021-10-13 12:04:51 -0300

    Create Blitz

--- lib/ex_mon.ex
+++ lib/ex_mon.ex
@@ -1,7 +1,13 @@
 defmodule ExMon do
-  alias ExMon.Player
-
+  alias ExMon.{Game,Player}
+  @computer_name "Blitz"
   def create_player(name, move_avg, move_rnd, move_heal) do
     Player.build(name, move_avg, move_rnd, move_heal)
+  end
+  
+  def start_game(player) do
+    @computer_name
+    |> create_player(:grab, :punch, :heal)
+    |> Game.start(player)
   end
 end

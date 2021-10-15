392d6704bd6eec853de90074dcc25540ba9be96b
Alexandre Pereira <lexandre32@gmail.com>
2021-10-14 23:17:53 -0300

    verify commit(Ash)

--- lib/ex_mon.ex
+++ lib/ex_mon.ex
@@ -1,6 +1,6 @@
 defmodule ExMon do
   alias ExMon.{Game,Player}
-  @computer_name "Blitz"
+  @computer_name "Ash"
   def create_player(name, move_avg, move_rnd, move_heal) do
     Player.build(name, move_avg, move_rnd, move_heal)
   end
